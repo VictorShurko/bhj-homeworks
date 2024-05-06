18749cb3394aff143085372c67f7963deecdd2aa
Victor Shurko <victorshurko@yandex.ru>
2024-05-06 22:30:06 +0300

    beautify JS reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,13 @@
-function toggleClass(){
-    document.querySelectorAll('.reveal').forEach(function(el){
-        let elementPosition = el.getBoundingClientRect();
-        
-        if (elementPosition.top <= window.innerHeight && elementPosition.bottom >= 0) {
-            el.classList.add('reveal_active');
-        } else { 
-            el.classList.remove('reveal_active');
-        }
-    });
+function toggleClass() {
+	document.querySelectorAll('.reveal').forEach(function(el) {
+		let elementPosition = el.getBoundingClientRect();
+
+		if (elementPosition.top <= window.innerHeight && elementPosition.bottom >= 0) {
+			el.classList.add('reveal_active');
+		} else {
+			el.classList.remove('reveal_active');
+		}
+	});
 }
 
 window.addEventListener('scroll', toggleClass);
