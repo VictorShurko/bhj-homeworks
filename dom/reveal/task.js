665c6a1c125acb61fa7daaf90559241e31653f17
Victor Shurko <victorshurko@yandex.ru>
2024-05-06 21:10:57 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,15 @@
+function toggleClass(){
+    document.querySelectorAll('.reveal').forEach(function(el){
+        let elementPosition = el.getBoundingClientRect();
+        
+        if (elementPosition.top <= window.innerHeight && elementPosition.bottom >= 0) {
+            el.classList.add('reveal_active');
+        } else { 
+            el.classList.remove('reveal_active');
+        }
+    });
+}
+
+window.addEventListener('scroll', toggleClass);
+
+document.addEventListener('DOMContentLoaded', toggleClass);
